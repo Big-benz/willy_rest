9a97a2553149f55316c3dadb564c61c4547357c0
Your Name <you@example.com>
2023-01-07 12:28:19 +0100

    changes

--- my_rest/settings.py
+++ my_rest/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,7 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STAICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-django_heroku.settings(locals())
 
 
 # Default primary key field type
